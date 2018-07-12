062010c187b181abedd95fb1bcfa1b3b9a496e85
peekee01 <peekee@gmail.com>
2018-07-12 23:10:45 +0200

    lesson-07

--- Smack/View/Constants.swift
+++ Smack/View/Constants.swift
@@ -8,8 +8,24 @@
 
 import Foundation
 
-// Segues
+typealias CompletionHandler = (_ Success: Bool) -> ()
 
+//URL Constants
+let BASE_URL = "https://peekeechats.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+
+// User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
+
