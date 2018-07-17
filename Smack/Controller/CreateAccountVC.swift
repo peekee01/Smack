f7d11ad001565aae82c6568851552a36b9c4fe04
peekee01 <peekee@gmail.com>
2018-07-17 23:18:28 +0200

    lesson 12 final

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -16,7 +16,9 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
-    
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,25 +32,32 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameTxt.text, usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         guard let pass = passTxt.text , passTxt.text != "" else { return }
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("User suggesfully registered")
+//                print("User suggesfully registered")
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("logged in user", AuthService.instance.authToken)
-                    }
-                })
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+//                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                            })
+                            
+                        }
+                    })
+                }
             }
+            
         }
-    
-    }
-    
-    @IBAction func closePressed(_ sender: Any) {
-        performSegue(withIdentifier: UNWIND, sender: nil)
-    }
-    
-    
+        
+        @IBAction func closePressed(_ sender: Any) {
+            performSegue(withIdentifier: UNWIND, sender: nil)
+        }
+        
+        
 }
---
+++ Smack/Services/UserDataService.swift
@@ -0,0 +1,33 @@
+//
+//  UserDataService.swift
+//  Smack
+//
+//  Created by Pieter Kuijsten on 17/07/2018.
+//  Copyright © 2018 Pieter Kuijsten. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserDataService(id: String, color: String, avatarName: String, email: String, name: String) {
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvatarName(avatarName: String) {
+        self.avatarName = avatarName
+    }
+    
+}
