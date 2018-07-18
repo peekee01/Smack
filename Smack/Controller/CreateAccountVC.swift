1587df7dfb0da3c653da3589a20ccdd61903bafb
peekee01 <peekee@gmail.com>
2018-07-18 22:34:02 +0200

    lesson 16

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -50,6 +50,7 @@ class CreateAccountVC: UIViewController {
         let b = CGFloat(arc4random_uniform(255)) / 255
         
         bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        avatarColor = "[\(r), \(g), \(b), 1]"
         UIView.animate(withDuration: 0.2) {
             self.userImg.backgroundColor = self.bgColor
         }
@@ -71,7 +72,7 @@ class CreateAccountVC: UIViewController {
                             if success {
                                 self.spinner.isHidden = true
                                 self.spinner.stopAnimating()
-                                print("User succesfully created and logged in with \(UserDataService.instance.name)")
+                                print("User succesfully created and logged in with username - \(UserDataService.instance.name)")
                                 self.performSegue(withIdentifier: UNWIND, sender: nil)
                                 NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                             }
