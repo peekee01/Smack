4dd60c7f2e305abadb5dc3b3fd5dcd8da74a2519
peekee01 <peekee@gmail.com>
2018-07-23 22:39:55 +0200

    lesson 26

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -68,7 +68,6 @@ class MessageService {
                             
                             let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
                             self.messages.append(message)
-                            print(self.messages)
                         }
                     }
                 } catch {
---
+++ Smack/View/MessageCell.swift
@@ -0,0 +1,33 @@
+//
+//  MessageCell.swift
+//  Smack
+//
+//  Created by Pieter Kuijsten on 23/07/2018.
+//  Copyright © 2018 Pieter Kuijsten. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+//        timeStampLbl.text = message.timeStamp
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+    
+    
+}
